aeca6ad014369f71652a311b63ef3960d2fac219
Grim <grimonpont.jeremy@gmail.com>
2017-09-05 12:23:27 +0200

    test

--- app/src/main/java/com/cdi/youpicityapp/MainActivity.java
+++ app/src/main/java/com/cdi/youpicityapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.cdi.youpicityapp;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.cdi.youpicityapp.fragment.SampleFragmentPagerAdapter;
@@ -22,5 +23,27 @@ public class MainActivity extends AppCompatActivity {
         PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         // Attach the view pager to the tab strip
         tabsStrip.setViewPager(viewPager);
+        tabsStrip.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            // This method will be invoked when a new page becomes selected.
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(MainActivity.this,
+                        "Selected page position: " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            // This method will be invoked when the current page is scrolled
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Code goes here
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Code goes here
+            }
+        });
     }
 }
--- app/src/main/java/com/cdi/youpicityapp/fragment/SampleFragmentPagerAdapter.java
+++ app/src/main/java/com/cdi/youpicityapp/fragment/SampleFragmentPagerAdapter.java
@@ -4,31 +4,38 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.astuetz.PagerSlidingTabStrip;
+import com.cdi.youpicityapp.R;
+
 /**
  * Created by Administrateur on 05/09/2017.
  */
 
-public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter  implements PagerSlidingTabStrip.IconTabProvider {
     final int PAGE_COUNT = 4;
-    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3", "Tab4" };
+    private int tabIcons[] = {R.drawable.ic_parcours2, R.drawable.ic_marker, R.drawable.ic_flag, R.drawable.ic_calendar};
 
     public SampleFragmentPagerAdapter(FragmentManager fm) {
+
         super(fm);
     }
 
     @Override
     public int getCount() {
+
         return PAGE_COUNT;
     }
 
     @Override
     public Fragment getItem(int position) {
+
         return PageFragment.newInstance(position + 1);
     }
 
     @Override
-    public CharSequence getPageTitle(int position) {
-        // Generate title based on item position
-        return tabTitles[position];
+    public int getPageIconResId(int position) {
+        return tabIcons[position];
     }
+
+
 }
